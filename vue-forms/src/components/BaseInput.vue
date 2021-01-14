161094883504ca120e4054d9597aa920df72dfa5
Paul Stey <paulstey@gmail>
2021-01-13 19:56:39 -0500

    first commit

---
+++ vue-forms/src/components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <label>{{ label }} </label>
+  <input 
+    :placeholder="label"
+    class="field"
+  >
+
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
