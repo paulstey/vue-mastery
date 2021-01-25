9b7cea72400b537485c6289057df7649fbd31956
paulstey <paulstey@gmail.com>
2021-01-25 12:16:00 -0500

    trying to debug vue 2/3 issue

--- mastering-vuex/src/store/index.js
+++ mastering-vuex/src/store/index.js
@@ -2,9 +2,30 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    user: { id: 'abc123', nane: 'Adam Jahr' }
-  },
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: ['sustainability',
+                 'nature',
+                 'animal welfare',
+                 'housing',
+                 'education',
+                 'food',
+                 'community'],
+    events: [{ id: 1, title: '...', organizer: '...' },
+             { id: 2, title: '...', organizer: '...' },
+             { id: 3, title: '...', organizer: '...' },
+             { id: 4, title: '...', organizer: '...' }
+    ]
+  
+    },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: (state) => (id) => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 });
